0da20a8e11a5864e793ca6521e2d34d1651dd5da
JAC <163740193+jean-albert@users.noreply.github.com>
2025-04-03 01:51:18 +0300

    Add files via upload

---
+++ golang-sprint/sprint/hello/abacus.go
@@ -0,0 +1,5 @@
+package sprint
+
+func Abacus(a int, b int) int {
+	result a / b
+}
---
+++ golang-sprint/sprint/hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	result := abacus(8, 3)
+	fmt.Println(result)
+}
